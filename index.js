6738b0c506ed210d523d4c7a5b6e7f0fa1d6b9c4
Mark Wanjau <theafrikanson@gmail.com>
2022-11-27 19:20:35 +0300

    due update assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    const caps = customerName.toUpperCase()
+    return caps
+}
+
+const setBestCustomer = () => {}
+
+const overwriteBestCustomer = () => {}
